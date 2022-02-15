ea8febc4b684a5d040aa516e987d78ffe93f0a89
Tomu Komatsu <tomu5642514@gmail.com>
2022-02-14 23:06:35 -0500

    03-ES6-Classes

--- React Tooling/03-ES6-Classes/lib/01_counter.js
+++ React Tooling/03-ES6-Classes/lib/01_counter.js
@@ -1,11 +1,22 @@
 class Counter {
   constructor(text) {
     // TODO: build an internal Map of word => occurrences.
+    this.mapOcurrences = new Map();
+    text.toLowerCase().split(" ").forEach((element) => {
+      if (this.mapOcurrences.get(element) === undefined) {
+        this.mapOcurrences.set(element, 0);
+      }
+      this.mapOcurrences.set(element, this.mapOcurrences.get(element) + 1);
+    });
   }
 
   occurrences(word) {
     // TODO: return the number of occurrences
+    let result = this.mapOcurrences.get(word.toLowerCase());
+    if (result === undefined) {
+      result = 0;
+    }
+    return result;
   }
 }
-
 module.exports = Counter;
--- React Tooling/03-ES6-Classes/lib/02_counter_arrow.js
+++ React Tooling/03-ES6-Classes/lib/02_counter_arrow.js
@@ -1,2 +1,28 @@
-
-
+const occurrences = (text) => {
+  const mapOcurrences = new Map();
+  text.toLowerCase().split(" ").forEach((element) => {
+    if (mapOcurrences.get(element) === undefined) {
+      mapOcurrences.set(element, 0);
+    }
+    mapOcurrences.set(element, mapOcurrences.get(element) + 1);
+  });
+  return {
+    mapOcurrences,
+    bar() {
+      const x = (word) => {
+        let result = this.mapOcurrences.get(word.toLowerCase());
+        if (result === undefined) {
+          result = 0;
+        }
+        return result;
+      };
+      return x;
+    }
+  };
+};
+function Counter(text) {
+  const obj = occurrences(text);
+  this.mapOcurrences = obj.mapOcurrences;
+  this.occurrences = obj.bar();
+}
+module.exports = Counter;
